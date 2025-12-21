33d6a433f9a1379e7b116c04ec084952b72172c5
Kyle Scoggin <243018999+ksco23@users.noreply.github.com>
2025-12-21 15:24:19 -0600

    Add playRound function
    Added code to play a single round. Also added some helper functions for
    computer or human wins.

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ let humanScore = 0;
 let computerScore = 0;
 
 //log(getComputerChoice());
-log(getHumanChoice());
+//log(getHumanChoice());
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
 
 //FUNCTION: getComputerChoice
     //Randomly return 'rock', 'paper,' or scissors'
@@ -29,9 +33,77 @@ function getHumanChoice(){
 
 //FUNCTION: playRound accepts 2 params, humanChoice and computerChoice
     //Convert humanChoice to lowercase
-    //Compare humanChoice and computerChoice to determine the round winner
+    //Compare humanChoice and computerChoice to determine the round winner (Rock beats scissors, scissors beat paper, and paper beats rock)
     //Log a string declaring the winner ('You lose! Paper beats Rock)
     //Increment humanScore or computerScore based on the round winner
+
+function playRound(humanChoice, computerChoice){
+    const humanChoiceLowercase = humanChoice.toLowerCase();
+
+    log(`Computer: ${computerChoice}\nYou: ${humanChoice}`);
+
+    switch(computerChoice){
+        case 'rock':
+            if(humanChoiceLowercase === 'scissors'){
+                //Computer wins
+                computerWins(computerChoice, humanChoice);
+            }
+            else if(humanChoiceLowercase === 'paper'){
+                //Human wins
+                humanWins(computerChoice, humanChoice);
+            }
+            else{
+                //Draw
+                log('It was a draw.');
+            }
+            break;
+        
+        case 'paper':
+            if(humanChoiceLowercase === 'rock'){
+                //Computer wins
+                computerWins(computerChoice, humanChoice);
+            }
+            else if(humanChoiceLowercase === 'scissors'){
+                //Human wins
+                humanWins(computerChoice, humanChoice);
+            }
+            else{
+                log('It was a draw.');
+            }
+            break;
+
+        case 'scissors':
+            if(humanChoiceLowercase === 'paper'){
+                //Computer wins
+                computerWins(computerChoice, humanChoice);
+            }
+            else if(humanChoiceLowercase === 'rock'){
+                //Human wins
+                humanWins(computerChoice, humanChoice);
+            }
+            else{
+                log('It was a draw.');
+            }
+            break;
+
+        default:
+            log('Something went wrong. Computer choice wasn\'t valid!');
+    }
+}
+
+function computerWins(computerChoice, humanChoice){
+    computerScore += 1;
+    log(`You lose! ${capFirstLetter(computerChoice)} beats ${capFirstLetter(humanChoice)}.`);
+}
+function humanWins(computerChoice, humanChoice){
+    humanScore += 1;
+    log(`You win! ${capFirstLetter(humanChoice)} beats ${capFirstLetter(computerChoice)}.`);
+}
+
+//Make the first letter of a string capitalized
+function capFirstLetter(string){
+    return string[0].toUpperCase() + string.slice(1);
+}
 
 //FUNCTION: playGame
     //Once playRound is working, move its declaration here, as well as the 2 global score variables
