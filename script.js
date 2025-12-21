4fda4dc3353056d869a3772cc985c7f6edb7b4d4
Kyle Scoggin <243018999+ksco23@users.noreply.github.com>
2025-12-20 18:18:18 -0600

    Write pseudocode for the project
    Created index.html and script.js. Read through
    the assignment and converted the assignment
    requirements to pseudocode.

---
+++ script.js
@@ -0,0 +1,24 @@
+const log = console.log;
+
+//VARIABLE: humanScore init to 0
+//VARIABLE: computerScore init to 0
+
+
+//FUNCTION: getComputerChoice
+    //Randomly return 'rock', 'paper,' or scissors'
+
+
+//FUNCTION: getHumanChoice
+    //Prompt the user for input, 'rock', 'paper,' or scissors'
+
+
+//FUNCTION: playRound accepts 2 params, humanChoice and computerChoice
+    //Convert humanChoice to lowercase
+    //Compare humanChoice and computerChoice to determine the round winner
+    //Log a string declaring the winner ('You lose! Paper beats Rock)
+    //Increment humanScore or computerScore based on the round winner
+
+//FUNCTION: playGame
+    //Once playRound is working, move its declaration here, as well as the 2 global score variables
+    //Play 5 rounds
+    //Declare a winner at the end
